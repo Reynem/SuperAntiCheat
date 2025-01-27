5ce5c74b0f25be34bf0b83600d51234d85acd387
Kerim_Lex <lexmeins1@gmail.com>
2025-01-27 21:12:13 +0500

    Transporting information to server

--- app/src/main/java/com/example/superanticheat/RegisterActivity.kt
+++ app/src/main/java/com/example/superanticheat/RegisterActivity.kt
@@ -2,10 +2,14 @@ package com.example.superanticheat
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 
 class RegisterActivity : AppCompatActivity() {
@@ -29,5 +33,31 @@ class RegisterActivity : AppCompatActivity() {
         val email: String = findViewById<View>(R.id.emailInput).toString()
         val name: String = findViewById<View>(R.id.nameInput).toString()
         val password: String = findViewById<View>(R.id.passwordInput).toString()
+        val registerButton: Button = findViewById(R.id.registration) // Убедитесь, что у вас есть такая кнопка в layout
+        registerButton.setOnClickListener {
+            if (email.isNotEmpty() && name.isNotEmpty() && password.isNotEmpty()) {
+                registerUser(name, email, password)
+            } else {
+                Toast.makeText(this, "Пожалуйста, заполните все поля", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun registerUser(username: String, email: String, password: String) {
+        lifecycleScope.launch {
+            try {
+                val user = User(username, email, password)
+                val response = RetrofitClient.apiService.registerUser(user)
+
+                if (response.isSuccessful) {
+                    Toast.makeText(this@RegisterActivity, "Регистрация успешна", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@RegisterActivity, "Ошибка регистрации", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: Exception) {
+                Toast.makeText(this@RegisterActivity, "Ошибка: ${e.message}", Toast.LENGTH_SHORT).show()
+                Log.e("RegisterActivity", "Error during registration", e)
+            }
+        }
     }
 }
