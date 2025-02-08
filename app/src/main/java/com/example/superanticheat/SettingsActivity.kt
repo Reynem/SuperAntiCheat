343c1a852bde2c3005261e506489b743b6f88272
Kerim_Lex <lexmeins1@gmail.com>
2025-02-08 23:38:36 +0500

    Created functions to manage user`s  nickname

--- app/src/main/java/com/example/superanticheat/SettingsActivity.kt
+++ app/src/main/java/com/example/superanticheat/SettingsActivity.kt
@@ -6,6 +6,10 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class SettingsActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +26,28 @@ class SettingsActivity: AppCompatActivity() {
         }
         val changeName: Button = findViewById(R.id.ChangeName)
 
+    }
+    fun updateUserName(newName: String) {
+        val token = "Bearer ${AuthManager.accessToken}"
+        val request = UpdateNameRequest(newName)
+
+        val call = RetrofitClient.apiService.updateName(token, request)
+
+        call.enqueue(object : Callback<UpdateNameResponse> {
+            override fun onResponse(call: Call<UpdateNameResponse>, response: Response<UpdateNameResponse>) {
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        AuthManager.nickname = it.new_name
+                        Toast.makeText(this@SettingsActivity, "Имя изменено!", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this@SettingsActivity, "Ошибка изменения имени", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<UpdateNameResponse>, t: Throwable) {
+                Toast.makeText(this@SettingsActivity, "Ошибка сети: ${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
