1d97db522a0a4a6681ebdc9bbce08721ab742182
Kerim_Lex <lexmeins1@gmail.com>
2025-02-14 23:46:53 +0500

    starting to change design

--- app/src/main/java/com/example/superanticheat/MainActivity.kt
+++ app/src/main/java/com/example/superanticheat/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -28,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
-        val cameraFunc: Button = findViewById(R.id.Camera)
+        val cameraFunc: ImageButton = findViewById(R.id.Camera)
         cameraFunc.setOnClickListener {
             if (AuthManager.isLoggedIn) {
                 val intent = Intent(this, CameraActivity::class.java)
